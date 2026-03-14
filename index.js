0b19636b0a21960ffad4a938ad641ce26a6c7b93
April Lindie <april@MacBookPro.lan>
2026-03-14 04:45:37 -0400

    complete combineUsers lab

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
+const datejs = require('datejs');
 
+function combineUsers(...args) {
+  const combinedObject = {
+    users: []
+  };
 
+  for (const array of args) {
+    combinedObject.users = [...combinedObject.users, ...array];
+  }
 
+  combinedObject.merge_date = new Date().toLocaleDateString('en-US');
+
+  return combinedObject;
+}
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
 };
